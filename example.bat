cd82b617a9e089e3578cab5a4889b454b8f13cbe
SantosJC <santos.je95@gmail.com>
2021-02-25 15:55:35 -0600

    Update example.bat
    changed

--- example.bat
+++ example.bat
@@ -1,7 +1,5 @@
 ECHO OFF
-cd >> "C:\Users\santo\OneDrive\Escritorio\TCRuta.txt"
-::cd "C:\TeamCity\buildAgent\work\647f93e3f0521871"
-gradle clean
-gradle build
+cd >> "C:\Users\santo\OneDrive\Escritorio\TCRuta.txt";
+gradle clean;
+gradle build;
 gradle test
-
